d69549b9543e66744f00509e317eeb4fc25b2c06
michalkryjan <michal.kryjan@gmail.com>
2023-01-13 14:54:01 +0100

    initial next-app created

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    printWidth: 100,
+    quoteProps: 'as-needed',
+    semi: true,
+    trailingComma: 'none',
+    arrowParens: 'avoid',
+    tabWidth: 4,
+    useTabs: false
+}
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Product Factory</title>
+                <meta name="description" content="Product Factory homepage" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <body>
+                <main></main>
+            </body>
+        </>
+    );
+}
